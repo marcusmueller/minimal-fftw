e1cea862cd434fe12abfd414fdba1afa0a083e63
Marcus Müller <mueller@kit.edu>
2017-10-05 18:55:28 +0200

    VOLKized mag²; made multi-FFT work

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ CFLAGS+=-O3
 all: minimal_fft
 
 minimal_fft: minimal_fft.c
-	cc --std=c99 -o minimal_fft -lfftw3f_threads -lfftw3f -lm --std=c99 ${CFLAGS} minimal_fft.c
+	${CC} --std=c99 -o minimal_fft -lfftw3f_threads -lfftw3f -lm `pkgconf --libs --cflags volk` --std=c99 ${CFLAGS} minimal_fft.c
 clean:
 	rm minimal_fft
--- minimal_fft.c
+++ minimal_fft.c
@@ -1,5 +1,6 @@
 #define _LARGEFILE64_SOURCE
 #define _POSIX_C_SOURCE 200112L
+#define _DEFAULT_SOURCE
 
 #include <sys/stat.h>
 #include <sys/mman.h>
@@ -11,6 +12,7 @@
 #include <complex.h>
 #include <math.h>
 #include <fftw3.h>
+#include <volk/volk.h>
 
 #define handle_error(msg) \
 	do { perror(msg); exit(EXIT_FAILURE); } while (0)
@@ -18,13 +20,13 @@
 int
 main(int argc, char *argv[])
 {
-	uint16_t *addr_in;
+  void *addr_in;
 	float *addr_out;
 	int fd_in;
 	int fd_out;
 	struct stat sb;
 	size_t length;
-	uint64_t fftlen;
+	int fftlen;
 	size_t num_ffts;
 	size_t index;
 
@@ -36,68 +38,71 @@ main(int argc, char *argv[])
 	fd_in = open(argv[1], O_RDONLY|O_LARGEFILE);
 	if (fd_in == -1)
 		handle_error("in open");
-	fd_out = open(argv[2], O_LARGEFILE|O_CREAT|O_TRUNC|O_RDWR, 0666);
+  if(posix_fadvise(fd_in, 0, 0, POSIX_FADV_SEQUENTIAL))
+    handle_error("in sequential");
+  fd_out = open(argv[2], O_LARGEFILE|O_CREAT|O_TRUNC|O_RDWR, 0666);
 	if (fd_out == -1)
 		handle_error("out open");
+  if(posix_fadvise(fd_in, 0, 0, POSIX_FADV_SEQUENTIAL))
+    handle_error("out sequential");
 
-	fftlen = atoll(argv[3]);
-	int nthreads = (argc == 5) ? 4 : atoi(argv[4]);
+	fftlen = atoi(argv[3]);
+	int nthreads = (argc <= 4) ? 4 : atoi(argv[4]);
 
-	posix_fallocate(fd_out, 0, fftlen* sizeof(float));
 
-	if (fstat(fd_in, &sb) == -1)           /* To obtain file size */
-		handle_error("fstat");
+  if (fstat(fd_in, &sb) == -1)           /* To obtain file size */
+    handle_error("fstat");
 
-	length = sb.st_size; //two bytes per short, two shorts per complex value
-	num_ffts = length/fftlen;
+  length = sb.st_size; //two bytes per short, two shorts per complex value
+  num_ffts = length/sizeof(fftwf_complex)/fftlen;
 
-	//map the input into memory.
-	addr_in = mmap(NULL, length, PROT_READ,
-			MAP_PRIVATE, fd_in, 0);
-	if (addr_in == MAP_FAILED)
-		handle_error("input mmap");
+  //map the input into memory.
+  addr_in = mmap(NULL, length, PROT_READ,
+                 MAP_PRIVATE, fd_in, 0);
+  if (addr_in == MAP_FAILED)
+    handle_error("input mmap");
 
-	//map the output into memory.
-	addr_out = mmap(NULL, fftlen*sizeof(float), PROT_WRITE,
-			MAP_SHARED, fd_out, 0);
-	if (addr_out == MAP_FAILED)
+  //map the output into memory.
+  addr_out = mmap(NULL, sizeof(float)*fftlen*num_ffts, PROT_WRITE,
+                  MAP_SHARED, fd_out, 0);
+  if (addr_out == MAP_FAILED)
 		handle_error("output mmap");
+	int fallocate_return = posix_fallocate(fd_out, 0, sizeof(float)*num_ffts*fftlen);
 
+  printf("address mapping %p, length %d, num_ffts %d, posix_fallocate said %d \n", addr_in, fftlen, num_ffts, fallocate_return);
 
 	//fftwf stuff
 	fftwf_init_threads();
 	fftwf_plan_with_nthreads(nthreads);
-	fftwf_complex *in, *out;
+	fftwf_complex *out;
 	fftwf_plan my_plan;
-	in = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex)*fftlen);
-	out = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex)*fftlen);
-	my_plan = fftwf_plan_dft_1d(fftlen, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+	out = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex)*fftlen*num_ffts);
+  int n[] = {fftlen};
+  my_plan = fftwf_plan_many_dft(1,            /* rank */
+                                n,     /* length */
+                                num_ffts,  /* how many */
+                                (fftwf_complex*)addr_in,    /* input pointer */
+                                n,       /* inembed */
+                                1,          /* istride */
+                                fftlen,     /* idist */
+                                out,        /* output pointer */
+                                n,       /* onembed */
+                                1,          /* ostride */
+                                fftlen,     /* odist */
+                                -1,          /* sign */
+                                FFTW_PRESERVE_INPUT | FFTW_ESTIMATE/* flags */
+                                );
 
-	float *fin = (float*)in;
-	float *fout = (float*)out;
-
-	for(index = 0; index < fftlen*2; index++) {
-		float tmp =  (float) addr_in[index];
-		fin[index] =tmp;
-	}
-	addr_in += 2 * fftlen;
 
 	//run that FFT
 	fftwf_execute(my_plan);
-
-	//calculate Power 
-	size_t currindex=0;
-	float real, imag;
-	for(index = 0; index < fftlen; index++)	{
-		real = fout[currindex++];
-		imag = fout[currindex++];
-		addr_out[index] = 10.0f*log10f( real * real + imag * imag );
-	}
-
 	fftwf_destroy_plan(my_plan);
-	fftwf_free(in);
+
+  volk_32fc_magnitude_squared_32f(addr_out,out, num_ffts*fftlen);
+
 	fftwf_free(out);
-	msync(addr_out, fftlen * sizeof(float), MS_SYNC);
+	if(msync(addr_out, sizeof(float)*num_ffts*fftlen, MS_SYNC))
+    handle_error("msync");
 
 	exit(EXIT_SUCCESS);
 }
